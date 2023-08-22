cbafbac44a109809650b5f2b9f9bf506dedb5064
Jimmy Lam <jimmy.lam@easycode-it.com>
2023-08-22 22:03:58 +0200

    feat: added basic form structure

--- src/app/features/add-blog-page/add-blog-page.component.ts
+++ src/app/features/add-blog-page/add-blog-page.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+type InvalidFormGroup = {
+  title: FormControl<string | null>;
+  content: FormControl<string | null>;
+};
 @Component({
   selector: 'app-add-blog-page',
   templateUrl: './add-blog-page.component.html',
   styleUrls: ['./add-blog-page.component.scss'],
 })
-export class AddBlogPageComponent {}
+export class AddBlogPageComponent implements OnInit {
+  form!: FormGroup<InvalidFormGroup>;
+
+  ngOnInit(): void {
+    this.form = new FormGroup<InvalidFormGroup>({
+      title: new FormControl<string | null>('an existing title', [
+        Validators.required,
+        Validators.pattern('^[A-Z]+(.)*'),
+      ]),
+      content: new FormControl<string | null>(null),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form);
+  }
+}
