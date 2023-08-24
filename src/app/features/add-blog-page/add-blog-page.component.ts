31b2df0a2ad0464b262f23078ca9a31fe47d532e
Jimmy Lam <jimmy.lam@easycode-it.com>
2023-08-24 20:03:02 +0200

    feat: added custom validator

--- src/app/features/add-blog-page/add-blog-page.component.ts
+++ src/app/features/add-blog-page/add-blog-page.component.ts
@@ -1,10 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  UntypedFormControl,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { BlogStateService } from './state/blog-state.service';
 
 type InvalidFormGroup = {
   title: FormControl<string | null>;
   content: FormControl<string | null>;
 };
+
+type CreatedBlog = {
+  title: string;
+  content: string;
+};
+
 @Component({
   selector: 'app-add-blog-page',
   templateUrl: './add-blog-page.component.html',
@@ -12,18 +27,56 @@ type InvalidFormGroup = {
 })
 export class AddBlogPageComponent implements OnInit {
   form!: FormGroup<InvalidFormGroup>;
+  valueChanges$!: Observable<
+    Partial<{ title: string | null; content: string | null }>
+  >;
+
+  constructor(public blogStateService: BlogStateService) {}
 
   ngOnInit(): void {
     this.form = new FormGroup<InvalidFormGroup>({
-      title: new FormControl<string | null>('an existing title', [
-        Validators.required,
-        Validators.pattern('^[A-Z]+(.)*'),
-      ]),
-      content: new FormControl<string | null>(null),
+      title: new FormControl<string | null>(
+        'an existing title',
+        [
+          Validators.required,
+          Validators.pattern('^[A-Z]+(.)*'),
+          customValidator,
+        ],
+        this.customAsyncValidator
+      ),
+      content: new FormControl<string | null>(null, Validators.required),
+    });
+
+    this.valueChanges$ = this.form.valueChanges;
+  }
+
+  customAsyncValidator(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        if (control.value === 'Test Async') {
+          resolve({ customAsync: true });
+        }
+        resolve(null);
+      }, 1000);
     });
   }
 
   onSubmit() {
     console.log(this.form);
+
+    if (this.form.valid) {
+      this.blogStateService.addBlog(this.form.value as CreatedBlog);
+    }
   }
 }
+
+const customValidator = (
+  control: UntypedFormControl
+): ValidationErrors | null => {
+  if (control.value === 'Test') {
+    return { custom: true }; //Fehler
+  }
+  return null;
+};
