3d19471306dc69e7473e374ea42a014ea7ffe380
Jimmy Lam <jimmy.lam@easycode-it.com>
2023-08-27 14:14:31 +0200

    feat: added sidebar with responsive design

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,7 +1,16 @@
-import { Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Output,
+  EventEmitter,
+  Input,
+  inject,
+} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { LoginResponse } from 'angular-auth-oidc-client';
+import { map, Observable, ReplaySubject } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+import { hasRole } from '../auth/jwt';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,6 +19,32 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class SidebarComponent {
   private breakpointObserver = inject(BreakpointObserver);
+  initials$: Observable<string>;
+  loginResponse$ = new ReplaySubject<LoginResponse | null>();
+
+  showCreateButton$: Observable<boolean>;
+
+  @Output('login') login$ = new EventEmitter();
+  @Output('logoff') logoff$ = new EventEmitter();
+
+  @Input() set loginResponse(value: LoginResponse | null) {
+    this.loginResponse$.next(value);
+  }
+
+  constructor() {
+    this.initials$ = this.loginResponse$.pipe(
+      map((response) =>
+        response?.userData?.preferred_username
+          .split(/[._-]/)
+          .map((token: string) => token.charAt(0))
+          .join('')
+      )
+    );
+
+    this.showCreateButton$ = this.loginResponse$.pipe(
+      map((response) => hasRole('user', response?.accessToken))
+    );
+  }
 
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
